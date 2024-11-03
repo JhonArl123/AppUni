04ff6eb68edebd22e65855d88d5da9d174077d07
JUAN ALFONSO RALIOS LARIOS <naujralios@gmail.com>
2024-11-02 22:04:44 -0600

    Add files via upload
    App red social para estudiantes universitarios

---
+++ projetFinaly/src/main/java/com/juan/projetfinaly/ProjetFinaly.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.juan.projetfinaly;
+
+/**
+ *
+ * @author RYZEN
+ */
+public class ProjetFinaly {
+
+    public static void main(String[] args) {
+        UniConecta ventana = new UniConecta();
+        ventana.setVisible(true);
+        
+        
+        
+        
+               
+    }
+}
---
+++ projetFinaly/src/main/java/com/juan/projetfinaly/UserDates.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.juan.projetfinaly;
+
+/**
+ *
+ * @author RYZEN
+ */
+public class UserDates {
+    
+    String Correo;
+    String carrera;
+    String semsestre;
+    String password;
+    String passwConf;
+
+    public UserDates() {
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getSemsestre() {
+        return semsestre;
+    }
+
+    public void setSemsestre(String semsestre) {
+        this.semsestre = semsestre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswConf() {
+        return passwConf;
+    }
+
+    public void setPasswConf(String passwConf) {
+        this.passwConf = passwConf;
+    }
+    
+    
+    
+           
+            
+    
+}
